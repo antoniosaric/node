9cee0932223f713a183074b1df8b76a2827c7b9c
Tony <saric.tony@gmail.com>
2017-07-05 12:37:40 -0700

    adds yargs

--- app.js
+++ app.js
@@ -4,20 +4,37 @@ var storage = require('node-persist');
 
 storage.initSync();
 
-// storage.setItemSync('accounts', [{
-//   username: 'tony',
-//   balance: 0
-// }]);
+function createAccount (account){
+  var accounts = storage.getItemSync('args');
 
-var accounts = storage.getItemSync('accounts');
+  if (typeof accounts === 'undefined'){
+    var accounts = [];
+  }
 
-// accounts.push({
-//   username: 'jen',
-//   balance: 75
-// });
+    accounts.push(account);
+    storage.setItemSync('accounts', accounts);
 
-// storage.setItemSync('accounts', accounts);
+    return account;
+}
 
+function getAccount(accountName){
+  var accounts = storage.getItemSync('accounts');
+  var matchedAccount;
 
-console.log(accounts);
+  accounts.forEach(function(account){
+    if (account.name === accountName){
+      matchedAccount = account;
+    }
+  });
 
+  return matchedAccount;
+}
+
+// createAccount({
+//   name: 'facebook',
+//   username: 'tony@something.com',
+//   password: 'password123!'
+// })
+
+var facebookAccount = getAccount('facebook');
+console.log(facebookAccount);
